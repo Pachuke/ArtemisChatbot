ea047bfb6556e0c93c596986404492ff527d9d0c
Stella <stellakoenings@gmail.com>
2023-06-28 15:34:37 -0500

    Initial Commit

---
+++ ArtemisChatbot/ArtemisChatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtemisChatbotApp.swift
+//  ArtemisChatbot
+//
+//  Created by Stella Koenings on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct ArtemisChatbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
